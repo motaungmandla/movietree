c6a43eef480697616bd51f281302f3b576d57f71
Motaung Mandla <maxphin21@gmail.com>
2024-08-29 19:23:37 +0200

    Update main_n.js

--- main_n.js
+++ main_n.js
@@ -1,14 +1,14 @@
-// alert("Greetings from Motaung Johannese Motaung");
+//alert("Greetings from Motaung Johannese Motaung");
 
-// Motaung Links to the content.
-const apiLink = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cb59f014c8c0f6911728fff3e3b277f2&page=1';  // Link to the API, works over the web only
+//Motaung Links to the content.
+
+const apiLink = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cb59f014c8c0f6911728fff3e3b277f2&page=1';  // link to the API, works over the web only
 const imgPath = 'https://image.tmdb.org/t/p/w1280';
 const searchApi = 'https://api.themoviedb.org/3/search/movie?&api_key=cb59f014c8c0f6911728fff3e3b277f2&query=';
 
 const userQuery = document.getElementById("section");
 const formInfo = document.getElementById("form");
 const search = document.getElementById("query");
-
 returnMovies(apiLink);
 
 function returnMovies(url) {
@@ -34,7 +34,7 @@ function returnMovies(url) {
                 title.setAttribute('id', 'title');
 
                 const mycenter = document.createElement('div');
-                mycenter.className = "centered"; // To access and style this later
+                mycenter.className = "centered"; //to access and style this later
 
                 title.innerHTML = `${element.title}`;
                 images.src = imgPath + element.poster_path;
@@ -44,21 +44,21 @@ function returnMovies(url) {
                 div_column.appendChild(div_card);
                 div_row.appendChild(div_column);
 
-                userQuery.appendChild(div_row);  // Main
+                userQuery.appendChild(div_row);  //main
 
                 // Add event listener to make the movie clickable
                 div_card.addEventListener('click', () => {
-                    //window.location.href = `details.html?id=${element.id}`;
-                    alert(`Contact Mandla about ${element.title`);
+                    alert(`You clicked on ${element.title}!`); 
+                    // You can redirect to a new page with more details or open a modal here
+                    // For example: window.location.href = `details.html?id=${element.id}`;
                 });
-
             });
         });
 }
 
-formInfo.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    userQuery.innerHTML = ''; // Remove old search results
+    userQuery.innerHTML = ''; // remove old search results 
 
     const searchItem = search.value;
     if (searchItem) {
@@ -66,4 +66,3 @@ formInfo.addEventListener("submit", (e) => {
         search.value = "";
     }
 });
-
